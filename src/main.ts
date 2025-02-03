57b862193fc2c8387265755afd5bf85991de5a24
MiguelLopez23 <miguel.lopez@tuvanosa.com>
2025-02-02 18:10:46 -0700

    cambios 3

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger('Bootstrap');
+
+  // Validar la variable de entorno MONGODB_URI
+  if (!process.env.MONGODB_URI) {
+    logger.error('La variable de entorno MONGODB_URI no está definida');
+    process.exit(1);
+  }
+
   const app = await NestFactory.create(AppModule);
 
-  // Habilitar CORS
   app.enableCors({
-    origin: true, // Permite todas las origenes en desarrollo
+    origin: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
@@ -32,6 +40,8 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(process.env.PORT || 3000, '0.0.0.0');
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  logger.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
